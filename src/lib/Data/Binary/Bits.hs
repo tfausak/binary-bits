d4435af4872c2f306c477abfca56a072db38a053
Taylor Fausak <taylor@fausak.me>
2020-07-27 21:50:27 -0400

    Qualify more imports

--- src/lib/Data/Binary/Bits.hs
+++ src/lib/Data/Binary/Bits.hs
@@ -1,34 +1,33 @@
--- | Parse and write bits easily. Parsing can be done either in a monadic style, or more
--- efficiently, using the 'Applicative' style. Writing is monadic style only.
--- See "Data.Binary.Bits.Get" and "Data.Binary.Bits.Put", respectively.
+-- | Parse and write bits easily. Parsing can be done either in a monadic
+-- style, or more efficiently, using the 'Applicative' style. Writing is
+-- monadic style only. See "Data.Binary.Bits.Get" and "Data.Binary.Bits.Put",
+-- respectively.
+module Data.Binary.Bits ( BinaryBit(putBits, getBits) ) where
 
-module Data.Binary.Bits where
-
-import Data.Binary.Bits.Get
-import Data.Binary.Bits.Put
-
-import Data.Word
+import qualified Data.Binary.Bits.Get as Get
+import qualified Data.Binary.Bits.Put as Put
+import qualified Data.Word as Word
 
 class BinaryBit a where
-  putBits :: Int -> a -> BitPut ()
-  getBits :: Int -> BitGet a
+  putBits :: Int -> a -> Put.BitPut ()
+  getBits :: Int -> Get.BitGet a
 
 instance BinaryBit Bool where
-  putBits _ = putBool
-  getBits _ = getBool
+  putBits _ = Put.putBool
+  getBits _ = Get.getBool
 
-instance BinaryBit Word8 where
-  putBits = putWord8
-  getBits = getWord8
+instance BinaryBit Word.Word8 where
+  putBits = Put.putWord8
+  getBits = Get.getWord8
 
-instance BinaryBit Word16 where
-  putBits = putWord16be
-  getBits = getWord16be
+instance BinaryBit Word.Word16 where
+  putBits = Put.putWord16be
+  getBits = Get.getWord16be
 
-instance BinaryBit Word32 where
-  putBits = putWord32be
-  getBits = getWord32be
+instance BinaryBit Word.Word32 where
+  putBits = Put.putWord32be
+  getBits = Get.getWord32be
 
-instance BinaryBit Word64 where
-  putBits = putWord64be
-  getBits = getWord64be
+instance BinaryBit Word.Word64 where
+  putBits = Put.putWord64be
+  getBits = Get.getWord64be
